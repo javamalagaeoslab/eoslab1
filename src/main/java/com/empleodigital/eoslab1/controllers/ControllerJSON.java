b9fe1709359f80f149d6b7172f052f48db315072
fcogritol <grimaret@gmail.com>
2017-03-13 09:12:52 +0100

    Add JSONCategorias

--- src/main/java/com/empleodigital/eoslab1/controllers/ControllerJSON.java
+++ src/main/java/com/empleodigital/eoslab1/controllers/ControllerJSON.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.empleodigital.eoslab1.bbdd.Conector;
+import com.empleodigital.eoslab1.beans.Categoria;
 import com.empleodigital.eoslab1.beans.ObjetoJSON;
 import com.empleodigital.eoslab1.beans.Producto;
 
@@ -39,6 +40,32 @@ public class ControllerJSON {
 		return json;
 		
 	}
+	
+	
+	@RequestMapping(value="JSONCategorias", method=RequestMethod.GET)
+	@ResponseBody 
+	public ObjetoJSON devuelveJSONCategorias(){
+		ArrayList<Categoria> seleccion = new ArrayList<Categoria>();
+		
+		//Conectamos con la BBDD usando la clase Conector creada anteriormente
+		JdbcTemplate jdbc = new JdbcTemplate(Conector.getDataSource());
+		
+		//Creo y ejecuto las consultas.
+		String sql;
+		sql ="SELECT * FROM categorias;";
+		
+		//Realizo la consulta que me devuelve todas las que tiene la base de datos
+		seleccion = (ArrayList<Categoria>) jdbc.query(
+				sql,
+				new BeanPropertyRowMapper<Categoria>(Categoria.class),
+				new Object[]{}
+				);
+		ObjetoJSON json = new ObjetoJSON(seleccion);
+		
+		return json;
+		
+	}
+	
 	
 
 	
